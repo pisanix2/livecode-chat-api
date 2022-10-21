b0cf2b622776009af9625f0c5ba9eca9719bd002
Pisani <pisanix@gmail.com>
2022-10-21 13:43:11 -0300

    feat: liberar cors para o front

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors();
   await app.listen(3001);
 }
 bootstrap();
